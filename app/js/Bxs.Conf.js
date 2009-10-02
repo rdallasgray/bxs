594a5431871c4cb6eaa9259b046e8de6a0c7a638
robot.johnston <robot.johnston@1c00e154-97e5-11de-b5b0-958946df26fc>
2009-10-02 16:12:12 +0000

    Configuration file
    git-svn-id: https://bxs.googlecode.com/svn/trunk@21 1c00e154-97e5-11de-b5b0-958946df26fc

---
+++ app/js/Bxs.Conf.js
@@ -0,0 +1,22 @@
+Bxs.Conf = {
+	
+	editor: {
+			mode: "none",
+			theme: "advanced",
+			content_css: Bxs.Url.app("/css/basic-html.css"),
+			gecko_spellcheck: true,
+			theme_advanced_toolbar_location: "top",
+			theme_advanced_toolbar_align: "left",
+			theme_advanced_buttons1: "print,|,cut,copy,paste,undo,redo,|,formatselect,bold,italic,underline,sub,sup,removeformat,|,bullist,numlist,blockquote,|,link,unlink",
+			theme_advanced_buttons2: "",
+			theme_advanced_buttons3: "",
+			plugins: "paste,print"
+	},
+	
+	css: {
+		basic: Bxs.Url.app("/css/basic-html.css"),
+		main: Bxs.Url.root("/css/main.css"),
+		default: Bxs.Url.app("/css/basic-html.css"),
+	}
+	
+};
