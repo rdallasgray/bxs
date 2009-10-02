d4c46270d4f6321810cae4b124b28fee35035424
robot.johnston <robot.johnston@1c00e154-97e5-11de-b5b0-958946df26fc>
2009-10-02 16:11:27 +0000

    Added to allow for abstraction of toolbar
    git-svn-id: https://bxs.googlecode.com/svn/trunk@18 1c00e154-97e5-11de-b5b0-958946df26fc

--- app/js/Bxs.Toolbar.Abstract.js
+++ app/js/Bxs.Toolbar.Abstract.js
@@ -5,7 +5,7 @@ Copyright (c) 2009 Robert Johnston
 This file is part of Boxes.
 
 Boxes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
- 
+
 Boxes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with Boxes. If not, see http://www.gnu.org/licenses/.
@@ -17,13 +17,16 @@ if (Bxs.Toolbar === undefined) {
 }
 
 Bxs.Toolbar.Abstract = function() {
-	
+
 };
 
-Bxs.Toolbar.Abstract.prototype = {
-	
-	getDomNode: function() {
-		return this.domNode;
+Bxs.Toolbar.Abstract.prototype = $.extend(true, {},
+
+	Bxs.Mixin.Stateable,
+
+	{
+		getDomNode: function() {
+			return this.domNode;
+		}
 	}
-	
-};
+);
