fabc6bc38a5274115f5aff7235a2219ca6b95211
robot.johnston <robot.johnston@1c00e154-97e5-11de-b5b0-958946df26fc>
2010-03-11 10:34:41 +0000

    Formatting
    git-svn-id: https://bxs.googlecode.com/svn/trunk@171 1c00e154-97e5-11de-b5b0-958946df26fc

--- app/js/Bxs.js
+++ app/js/Bxs.js
@@ -220,7 +220,7 @@ Bxs = {
 	
 	error: {
 		fatal: function(msg) {
-			// TODO make sure all errors are notified (by POSTING to server://base/errors ?), flash just one alert, and shut down
+			// TODO make sure all errors are notified (by POSTING to server://base/errors ?)
 			Bxs = null;
 			try {
 				console.debug(msg);
