f186e76264ff52668c4889a8a253222edeef51ca
robot.johnston <robot.johnston@1c00e154-97e5-11de-b5b0-958946df26fc>
2011-04-27 09:05:27 +0000

    Added getListUrl method.
    git-svn-id: https://bxs.googlecode.com/svn/trunk@235 1c00e154-97e5-11de-b5b0-958946df26fc

--- app/js/Bxs.Association.js
+++ app/js/Bxs.Association.js
@@ -34,6 +34,37 @@ Bxs.Association = {
 	
 	getModelName: function(columnName, viewAttrs) {
 		return Bxs.Inflector.pluralize(Bxs.Association.getName(columnName, viewAttrs));
+	},
+	
+	getListUrl: function(modelName, view) {
+		var listUrl = "/" + modelName;
+		// Get the listed model's schema
+		Bxs.Ajax.getSchema(listUrl, function(columnSchema) {
+			var parentSchema = view.controller.schema,
+				observedSchema = (view.attrs.observing === undefined) ? {} : view.getObservedBox().controller.schema,
+				associateKeys = [],
+				sharedKey,
+				defaultColumn,
+				associateId,
+				row = null;
+			
+			// Check each key of the listed model's schema -- if it's an association, keep it for later checking
+			$.each(columnSchema, function(key) { if (Bxs.Column.isAssociation(key)) associateKeys.push(key) });
+			// If matching keys are found in the list schema and parent schema, use that key to construct the list's url
+			if (!!(sharedKey = associateKeys.filter(function(el) el in parentSchema)[0])) {
+				row = view.getSelectedRow();
+			}
+			else if (!!(sharedKey = associateKeys.filter(function(el) el in observedSchema)[0])) {
+				row = view.getObservedBox().view.getSelectedRow();
+			}
+			if (row !== null) {
+				associateId = $(row).find("[name='"+sharedKey+"']").attr("value");
+				var keyModelName = Bxs.Inflector.pluralize(Bxs.Association.getName(sharedKey));
+				listUrl = "/" + keyModelName + "/" + associateId + "/" + modelName;
+			}
+			$(Bxs.eventsPublisher).trigger("gotListUrl." + modelName + '-' + view.attrs.id, 
+				[{ url: listUrl, key: sharedKey, id: associateId }]);
+		});
 	}
 	
 };
